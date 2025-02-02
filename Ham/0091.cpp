9f529b97c016bb10912b30d7795653b2957c72f3
Lyhoangem <lyhoangem1508@gmail.com>
2025-02-02 16:46:48 +0700

    0092

--- Ham/0091.cpp
+++ Ham/0091.cpp
@@ -1,21 +1,30 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
-
-double tong(int a, int b){
-	return a+b;
+void sao(int d, int r){
+	
+	for(int i=1;i<=2*r-1;i++){
+		if(i==1){
+			for(int j=1;j<=d;j++) cout<<"*";
+		}
+		if(i%2==0) cout<<endl;
+		if(i!=1 && i%2!=0 && i!=2*r-1){
+			for(int j=1;j<=d;j++){
+				if(j==1) cout<<"*";
+				if(j!=1 && j!=d) cout<<" ";
+				if(j==d) cout<<"*";
+			}
+		}
+		if(i==2*r-1){
+			for(int j=1;j<=d;j++){
+				cout<<"*";
+			}
+		}
+	}
 }
-
-double chia(int a, int b){
-	double k =(double)a/b;
-}
-
 
 int main(){
-	int a,b,c;
-	cin>>a>>b>>c;
-	cout<<tong(a,b)+c;
-	cout<<fixed<<setprecision(2)<<chia(a,b)+c;
-	cout<<fixed<<setprecision(3)<<chia(a,tong(b,c));
-	
+	int d,r;
+	cin>>d>>r; 
+	sao(d,r);
 } 
